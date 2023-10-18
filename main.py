98d05c1126b1370eda08aba493ae7fa41e65bd32
georgebrnv <barinov.egor2013@gmail.com>
2023-10-17 22:18:06 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+from tkinter import *
+from tkinter import messagebox
+from screeninfo import get_monitors
+import pyperclip
+
+FONT = "Courier"
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    password_entry.delete(0, END)
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    # Insert generated password into Password entry box
+    password_entry.insert(0, password)
+
+    # Add password to clipboard (copy)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email_username = email_username_entry.get()
+    password = password_entry.get()
+
+    # Check if entry boxes are empty
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showwarning(title="Oops", message="Please fill up all the fields.")
+    else:
+        # Pop-up confirmation window
+        is_ok = messagebox.askokcancel(title=website, message=f"Is this correct info?\n Email: {email_username}\n Password: {password}\n"
+                                                      f"Do you want to save it?")
+        # User presses "ok"
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.writelines(f"{website} | {email_username} | {password}\n")
+            # delete previous entry content
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            # put cursor in website entry box
+            website_entry.focus()
+
+# ---------------------------- UI SETUP ------------------------------- #
+# User's screen info
+screens = get_monitors()
+screen_width = screens[0].width
+screen_height = screens[0].height
+
+# Window set up
+window = Tk()
+window.title("Password Manager App")
+window.config(padx=20, pady=20)
+screen_x = int((screen_width - window.winfo_width()) // 2)
+screen_y = int((screen_height - window.winfo_height()) // 2)
+window.geometry(f"+{screen_x}+{screen_y}")
+
+# Canvas and png
+canvas = Canvas(width=200, height=200)
+logo_png = PhotoImage(file="logo.png")
+canvas_image = canvas.create_image(100, 100, image=logo_png)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_text = Label(text="Website: ", font=(FONT, 14))
+website_text.grid(column=0, row=1)
+email_username_text = Label(text="Email/Username: ", font=(FONT, 14))
+email_username_text.grid(column=0, row=2)
+password_text = Label(text="Password: ", font=(FONT, 14))
+password_text.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry(width=36)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_username_entry = Entry(width=36)
+email_username_entry.grid(column=1, row=2, columnspan=2)
+email_username_entry.insert(0, "example@email.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", width=11, command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=34, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
+
+window.mainloop()
